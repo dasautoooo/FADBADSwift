ca7e78144b109de5f974e48d08316290e8f50b10
Leonard Chan <wxclx98@gmail.com>
2024-12-30 21:08:33 -0500

    Clear Package.swift

--- Package.swift
+++ Package.swift
@@ -6,14 +6,11 @@ import PackageDescription
 let package = Package(
     name: "FADBADSwift",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FADBADSwift",
             targets: ["FADBADSwift"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "FADBADSwift",
             dependencies: ["fadbadxx"],
